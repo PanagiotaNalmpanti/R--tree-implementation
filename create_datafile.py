5078d5c733a3e399543880535250b7295257e366
Valia-p <pantelvp@csd.auth.gr>
2024-04-21 13:42:37 +0300

    done with datafile

--- create_datafile.py
+++ create_datafile.py
@@ -27,8 +27,32 @@ def createBlocks(record_data):
 
     block0 = (len(record_data), len(listOfBlocks))
     listOfBlocks[0] = block0
-    print(block0)
-    print(len(listOfBlocks[20]))
+    # print(block0)
+    # print(len(listOfBlocks[1]))
+
+    return listOfBlocks
+
+
+def create_xml(blocks, records, xml):
+    Blocks = ET.Element("Blocks")
+    block0 = ET.SubElement(Blocks, "Block", id=str(0))
+    ET.SubElement(block0, "number_of_records").text = str(records)
+    ET.SubElement(block0, "number_of_blocks").text = str(len(blocks))
+
+    for i, block in enumerate(blocks):
+        if i != 0:
+            blockEl = ET.SubElement(Blocks, "Block", id=str(i))
+
+            for j, record in enumerate(block):
+                recordEl = ET.SubElement(blockEl, "Record", id=str(j))
+                ET.SubElement(recordEl, "id").text = str(record[0])
+                ET.SubElement(recordEl, "name").text = str(record[1])
+                ET.SubElement(recordEl, "coordinates").text = " ".join(map(str, record[2:])) # map: Applies str in each element of the record[2:] = coordinates
+
+    tree = ET.ElementTree(Blocks)
+
+    # write the tree into an XML file
+    tree.write("datafile.xml", encoding='utf-8', xml_declaration=True)
 
 
 # parse the .osm XML file
@@ -47,7 +71,9 @@ for element in root:
             tags[tag.attrib["k"]] = tag.attrib["v"]
         name = tags.get("name", "unknown")
 
-        record_data.append([id, name, (lat, lon)])
+        # coordinates = (lat,lon)
+        record_data.append([id, name, lat, lon])
 
-#print(record_data)
-createBlocks(record_data)
+# print(record_data)
+listOfBlocks = createBlocks(record_data)
+create_xml(listOfBlocks, len(record_data), "datafile.xml")
