b5d76921db1d0d2a2b2b1ccb3afd1806ddbc2e88
vaspn <pantelvp@csd.auth.gr>
2024-07-23 13:59:08 +0300

    knn done

--- KNN.py
+++ KNN.py
@@ -1,9 +1,67 @@
+import heapq
+import math
+import time
+
 from Entry import LeafEntry, Entry, Rectangle
 import xml.etree.ElementTree as ET
 from Node import Node
 
-def knn(root, point, k):
-    pq = []
+
+def knn(root, qpoint, k):
+    pq = []  # Elements are (distance, count, node/point, is_leaf)
+    count = 0  # used to maintain the order of elements with the same distance in the queue
+    visited = set()  # To keep track of visited nodes and leaf entries
+
+    # Initialize the queue with the root
+    for entry in root.entries:
+        if id(entry) not in visited:
+            visited.add(id(entry))
+
+            if isinstance(entry, LeafEntry):
+                point_dist = math.sqrt(sum([(a - b) ** 2 for a, b in zip(qpoint, entry.point)]))
+                heapq.heappush(pq, (point_dist, count, entry, True))
+            else:
+                heapq.heappush(pq, (entry.rectangle.euclidean_distance(qpoint), count, entry.child, False))
+            count += 1
+
+    result = []
+    while pq and len(result) < k:
+        distance, _, current, is_leaf = heapq.heappop(pq)
+        if is_leaf:
+            result.append((distance, current.point, current.record_id))
+        else:
+            for entry in current.entries:
+                entry_id = id(entry)
+                if entry_id not in visited:
+                    if isinstance(entry, LeafEntry):
+                        point_dist = math.sqrt(sum([(a - b) ** 2 for a, b in zip(qpoint, entry.point)]))
+                        heapq.heappush(pq, (point_dist, count, entry, True))
+                    else:  # Internal Node
+                        rectangle_distance = entry.rectangle.euclidean_distance(qpoint)
+                        heapq.heappush(pq, (rectangle_distance, count, entry.child, False))
+                    count += 1
+    return result
+
+
+def linear_search_in_datafile_KNN(qpoint, datafile, k):
+    tree = ET.parse(datafile)
+    root = tree.getroot()
+    result = []
+
+    for block_elem in root.findall("Block"):
+        if int(block_elem.get("id")) == 0:
+            continue
+
+        # for every record in each block
+        for record_elem in block_elem.findall("Record"):
+            coordinates = record_elem.find(".//coordinates").text.split()
+            point = list(map(float, coordinates)) # Converts the list of strings to a list of floats representing the point.
+            point_dist = math.sqrt(sum([(a - b) ** 2 for a, b in zip(qpoint, point)]))
+            result = [point, point_dist]
+            result.append(result)
+
+        return result[:k]
+
 
 def load_rtree_from_xml(filename):
     rtree = ET.parse(filename)
@@ -59,3 +117,35 @@ def load_rtree_from_xml(filename):
         Node.set_overflow_treatment_level(nodes[-1].getLevel())
 
     return nodes
+
+
+tree = load_rtree_from_xml("indexfile1.xml")
+
+length = len(tree[-1].entries[0].point)
+print(length)
+
+#qpoint = [0] * length
+qpoint = [3,-8]
+k = 1
+
+start_time = time.time()
+k_nearest_neighbors = knn(tree[0], qpoint, k)
+end_time = time.time()
+print("\nKNN using knn algorithm in tree: ", end_time-start_time, " sec")
+
+for distance, point, record_id in k_nearest_neighbors:
+    print(f"Distance: {distance}")
+    print(f"RecordID: {record_id}")
+    print(f"Point: {point}")
+    print("---------------------")
+
+datafile = "datafile.xml"
+start_time = time.time()
+result_records = linear_search_in_datafile_KNN(qpoint, datafile, k)
+end_time = time.time()
+print("\nKNN using linear search in datafile: ", end_time-start_time, " sec")
+
+for list in result_records:
+    print(f"Distance: {list[1]}")
+    print(f"Point: {list[0]}")
+    print("---------------------")
